abbcb9adb37d720690923122529646da89c6a92e
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2024-04-15 05:08:11 -0300

    Atividade: Implementando o efeito de som nos botões

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("Menu UI properties")] 
-    [SerializeField] private Button startButton;
+    [Header("Menu UI properties")] [SerializeField]
+    private Button startButton;
+
     [SerializeField] private Button optionsButton;
     [SerializeField] private Button exitButton;
 
@@ -19,16 +20,20 @@ public class UIManager : MonoBehaviour
 
     private void GoToGameplayScene()
     {
+        GameManager.Instance.AudioManager.PlaySFX(SFX.ButtonClick);
         SceneManager.LoadScene("Gameplay");
     }
 
     private void OpenOptionsMenu()
     {
+        GameManager.Instance.AudioManager.PlaySFX(SFX.ButtonClick);
+        Debug.LogWarning("Botão de opções ainda não está implementado");
         //TODO: Criar menu de opções
     }
 
     private void ExitGame()
     {
+        GameManager.Instance.AudioManager.PlaySFX(SFX.ButtonClick);
 //If de compilação        
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
